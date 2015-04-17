e260286d794c884a3e32764c5536b6c17ba9ccb3
id774 <idnanashi@gmail.com>
2015-04-17 16:27:42 +0900

    7 Update cp option of deploy script

--- deploy_fluent_scripts.sh
+++ deploy_fluent_scripts.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-sudo cp $HOME/hotnews/zscore_daily.py /home/fluent/.fluent/bin/zscore.py
-sudo cp $HOME/hotnews/fluentd_wordcount.rb /home/fluent/.fluent/bin/
-sudo cp $HOME/hotnews/news_hottopic.rb /home/fluent/.fluent/bin/
-sudo cp $HOME/hotnews/mongo_fluentd_wordcount.rb /home/fluent/.fluent/bin/
-sudo cp $HOME/hotnews/mongo_news_hottopic.rb /home/fluent/.fluent/bin/
-sudo cp $HOME/hotnews/wordcount_exclude.txt /home/fluent/.fluent/log/
-sudo cp $HOME/hotnews/db_wordcount.rb /home/fluent/.fluent/bin/
-sudo cp $HOME/hotnews/db_news_hottopic.rb /home/fluent/.fluent/bin/
+sudo cp -uv $HOME/hotnews/zscore_daily.py /home/fluent/.fluent/bin/zscore.py
+sudo cp -uv $HOME/hotnews/fluentd_wordcount.rb /home/fluent/.fluent/bin/
+sudo cp -uv $HOME/hotnews/news_hottopic.rb /home/fluent/.fluent/bin/
+sudo cp -uv $HOME/hotnews/mongo_fluentd_wordcount.rb /home/fluent/.fluent/bin/
+sudo cp -uv $HOME/hotnews/mongo_news_hottopic.rb /home/fluent/.fluent/bin/
+sudo cp -uv $HOME/hotnews/wordcount_exclude.txt /home/fluent/.fluent/log/
+sudo cp -uv $HOME/hotnews/db_wordcount.rb /home/fluent/.fluent/bin/
+sudo cp -uv $HOME/hotnews/db_news_hottopic.rb /home/fluent/.fluent/bin/
 sudo chown -R fluent:fluent /home/fluent/.fluent/bin/
 sudo chown -R fluent:fluent /home/fluent/.fluent/log/wordcount_exclude.txt
 sudo chmod 660 /home/fluent/.fluent/bin/*
